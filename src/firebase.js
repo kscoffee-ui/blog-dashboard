79fd0555b6833a899265b201bc863c489eeef57c
Your Name <you@example.com>
2026-04-06 14:37:06 +0900

    fix: firebase init

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,10 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBTsRmYeZe8pGcBvGRRLi1Rhey6AKojQRk",
   authDomain: "blog-dashboard-f267b.firebaseapp.com",
-  projectId: "blog-dashboard-f267b"
+  projectId: "blog-dashboard-f267b",
+  storageBucket: "blog-dashboard-f267b.firebasestorage.app",
+  messagingSenderId: "806855293683",
+  appId: "1:806855293683:web:5041d6daacf97cf4a59586"
 };
 
 const app = initializeApp(firebaseConfig);
